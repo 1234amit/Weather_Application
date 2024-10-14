566114156f16b3fc5b27bde80590720d13749e27
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2024-10-15 01:20:02 +0600

    first commit

---
+++ src/Components/WeatherApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./WeatherApp.css";
+
+const WeatherApp = () => {
+  return (
+    <>
+      <div>WeatherApp</div>
+    </>
+  );
+};
+
+export default WeatherApp;
